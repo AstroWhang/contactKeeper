4326c680ba8b520cf24127ffb4f047ea117f0550
astrowhang <whangastro@gmail.com>
2019-08-11 12:18:00 -0700

    Express server setup with inital routes

---
+++ server.js
@@ -0,0 +1,15 @@
+// bringing in express, can't use import without implemeneting babel, using common js syntax
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper Api...'}));
+
+// DEFINE Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
